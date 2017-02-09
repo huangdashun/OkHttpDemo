ae61b05b13fd3060b5ed9ebf1a8f9c5b7e5033c3
dashunzi <406846759@qq.com>
2017-02-09 11:40:43 +0800

    add get and post request

--- app/src/main/java/com/example/administrator/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/okhttpdemo/MainActivity.java
@@ -1,13 +1,98 @@
 package com.example.administrator.okhttpdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout activityMain;
+    private Button sendRequest;
+    private TextView responseText;
+
+    private void assignViews() {
+        activityMain = (LinearLayout) findViewById(R.id.activity_main);
+        sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //使用get的方式
+                //sendRequestWithOkHttpGet();
+                //是用post的方式
+                sendRequestWithOkHttpPost();
+            }
+        });
+    }
+
+    //使用get的方式
+    private void sendRequestWithOkHttpGet() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder().url("http://www.baidu.com").build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    String responseData = response.body().string();
+                    showResponse(responseData);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //使用post的方式
+    private void sendRequestWithOkHttpPost() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormBody.Builder()
+                        .add("username", "admin")
+                        .add("password", "123456")
+                        .build();
+                Request request = new Request.Builder().url("http://www.baidu.com")
+                        .post(requestBody)
+                        .build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    String responseData = response.body().string();
+                    showResponse(responseData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    private void showResponse(final String responseData) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseText.setText(responseData);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
     }
+
 }
